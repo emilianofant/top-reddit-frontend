d96c5a400c6ee5bbfdd583fcbd74c7d5921ad1e8
Emiliano Fant <emilianofant@github.com>
2021-01-24 12:39:02 -0300

    feat: initial setup done

---
+++ src/modules/main/container/Main.tsx
@@ -0,0 +1,11 @@
+import { FunctionComponent } from 'react';
+
+const MainContainer: FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Reddit Top Post discover</h1>
+    </div>
+  );
+};
+
+export default MainContainer;
